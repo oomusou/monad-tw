0f4d6f4b109a7c519fc797094835ff084494c823
oomusou xiao <oomusou@gmail.com>
2020-10-03 20:23:33 +0800

    Format components/Modal/navabarMobileModal.vue

--- src/components/Modal/NavbarMobileModal.vue
+++ src/components/Modal/NavbarMobileModal.vue
@@ -4,31 +4,11 @@
       <h2 class="text-xl my-0">Navigation</h2>
       <div class="menu-links">
         <ul>
-          <li
-            v-for="navItem in $static.metadata.headerNavigation"
-            :key="navItem.name"
-            class="py-1"
-          >
-            <g-link
-              class="block py-1"
-              :to="navItem.link"
-              :title="navItem.name"
-              v-if="navItem.external!=true && navItem.children.length <=0"
-            >{{ navItem.name}}</g-link>
-            <a
-              class="block"
-              :href="navItem.link"
-              target="_blank"
-              :title="navItem.name"
-              v-if="navItem.external==true && navItem.children.length <=0"
-            >{{ navItem.name}}</a>
+          <li v-for="navItem in $static.metadata.headerNavigation" :key="navItem.name" class="py-1">
+            <g-link class="block py-1" :to="navItem.link" :title="navItem.name" v-if="navItem.external!=true && navItem.children.length <=0">{{ navItem.name}}</g-link>
+            <a class="block" :href="navItem.link" target="_blank" :title="navItem.name" v-if="navItem.external==true && navItem.children.length <=0">{{ navItem.name}}</a>
             <ClientOnly>
-              <v-popover
-                placement="right"
-                popoverClass="mobile-navbar-popover"
-                offset="16"
-                v-if="navItem.children.length > 0"
-              >
+              <v-popover placement="right" popoverClass="mobile-navbar-popover" offset="16" v-if="navItem.children.length > 0">
                 <a class="block py-1" style="cursor:pointer;">
                   {{ navItem.name }}
                   <font-awesome :icon="['fas', 'angle-right']"></font-awesome>
@@ -36,24 +16,9 @@
 
                 <template slot="popover">
                   <ul>
-                    <li
-                      v-for="subItem in navItem.children"
-                      :key="subItem.name"
-                      class="px-4 py-2 submenu-item hover:text-white"
-                    >
-                      <g-link
-                        class="block"
-                        :to="subItem.link"
-                        :title="subItem.name"
-                        v-if="subItem.external!=true"
-                      >{{ subItem.name}}</g-link>
-                      <a
-                        class="block"
-                        :href="subItem.link"
-                        target="_blank"
-                        :title="subItem.name"
-                        v-if="subItem.external==true"
-                      >{{ subItem.name}}</a>
+                    <li v-for="subItem in navItem.children" :key="subItem.name" class="px-4 py-2 submenu-item hover:text-white">
+                      <g-link class="block" :to="subItem.link" :title="subItem.name" v-if="subItem.external!==true">{{ subItem.name}}</g-link>
+                      <a class="block" :href="subItem.link" target="_blank" :title="subItem.name" v-if="subItem.external===true">{{ subItem.name}}</a>
                     </li>
                   </ul>
                 </template>
@@ -68,7 +33,6 @@
     </div>
   </div>
 </template>
-
 
 <static-query>
 query {
@@ -87,15 +51,13 @@ query {
   }
 }
 </static-query>
+
 <script>
-import Subnavigation from "~/components/Navbar/NavbarSubNavigation.vue";
+import Subnavigation from '@/components/Navbar/NavbarSubNavigation.vue'
 
 export default {
   components: {
     Subnavigation
   }
-};
+}
 </script>
-
-<style>
-</style>
