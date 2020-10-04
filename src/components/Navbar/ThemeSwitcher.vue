9cdf50f79f9e8f7f8c2f7613a3ec841cd169fd92
oomusou xiao <oomusou@gmail.com>
2020-10-04 09:55:53 +0800

    Format components/Navbar/ThemeSwitcher.vue

--- src/components/Navbar/ThemeSwitcher.vue
+++ src/components/Navbar/ThemeSwitcher.vue
@@ -1,10 +1,5 @@
 <template>
-  <a role="button" @click.prevent="toggleTheme()"  
-      :aria-label="'Toggle ' + nextTheme" 
-      :title="'Toggle ' + nextTheme"
-      class="toggle-theme"
-    >
-    
+  <a role="button" @click.prevent="toggleTheme()" :aria-label="'Toggle ' + nextTheme" :title="'Toggle ' + nextTheme" class="toggle-theme">
     <font-awesome :icon="['fas', 'sun']" v-if="theme === 'dark'"></font-awesome>
     <font-awesome :icon="['fas', 'moon']" v-if="theme === 'light'"></font-awesome>
   </a>
@@ -13,32 +8,38 @@
 <script>
 let themes = ['light', 'dark']
 
+let nextTheme = function() {
+  let currentIndex = themes.indexOf(this.theme)
+  let nextIndex = (currentIndex + 1) % themes.length
+  return themes[nextIndex]
+}
+
+let toggleTheme = function() {
+  const currentIndex = themes.indexOf(this.theme)
+  const nextIndex = (currentIndex + 1) % themes.length
+  window.__setPreferredTheme(themes[nextIndex])
+
+  this.$emit('setTheme', themes[nextIndex])
+}
+
+let mounted = function() {
+  // set default
+  if (typeof window.__theme !== 'undefined')
+    this.$emit('setTheme', window.__theme)
+}
+
 export default {
   props: {
     theme: {
       type: String,
     },
   },
-
   computed: {
-    nextTheme() {
-      const currentIndex = themes.indexOf(this.theme)
-      const nextIndex = (currentIndex + 1) % themes.length
-      return themes[nextIndex]
-    }
+    nextTheme
   },
   methods: {
-    toggleTheme() {
-      const currentIndex = themes.indexOf(this.theme);
-      const nextIndex = (currentIndex + 1) % themes.length;
-      window.__setPreferredTheme(themes[nextIndex])
-
-      this.$emit('setTheme', themes[nextIndex])
-    }
+    toggleTheme
   },
-  async mounted() {
-    // set default
-    if (typeof window.__theme !== 'undefined') this.$emit('setTheme', window.__theme)
-  }
+  mounted
 }
 </script>
