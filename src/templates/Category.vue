bdfaec6b61577770e7c737cddc87ec0c56eb8188
oomusou xiao <oomusou@gmail.com>
2020-10-04 11:01:18 +0800

    Format templates/Category.vue

--- src/templates/Category.vue
+++ src/templates/Category.vue
@@ -4,71 +4,35 @@
 
     <div class="container mx-auto">
       <div class="flex flex-wrap my-4">
-        <CardItem
-          v-for="edge in $page.category.belongsTo.edges"
-          :key="edge.node.id"
-          :record="edge.node"
-        />
+        <CardItem v-for="edge in $page.category.belongsTo.edges" :key="edge.node.id" :record="edge.node"/>
       </div>
 
       <div class="pagination flex justify-center mb-8">
-        <Pagination
-          :baseUrl="$page.category.path"
-          :currentPage="$page.category.belongsTo.pageInfo.currentPage"
-          :totalPages="$page.category.belongsTo.pageInfo.totalPages"
-          :maxVisibleButtons="5"
-          v-if="$page.category.belongsTo.pageInfo.totalPages > 1"
-        />
+        <Pagination :baseUrl="$page.category.path" :currentPage="$page.category.belongsTo.pageInfo.currentPage" :totalPages="$page.category.belongsTo.pageInfo.totalPages" :maxVisibleButtons="5" v-if="$page.category.belongsTo.pageInfo.totalPages > 1"/>
       </div>
     </div>
   </Layout>
 </template>
 
-<page-query>
-  query($id: ID!, $page:Int) {
-    category(id: $id) {
-      title
-      path
-      belongsTo(perPage: 6, page: $page) @paginate {
-        totalCount
-        pageInfo {
-          totalPages
-          currentPage
-        }
-        edges {
-          node {
-            ... on Blog {
-              id
-              title
-              image(width: 800)
-              path
-              timeToRead
-              featured
-              humanTime: created(format: "DD MMM YYYY")
-              datetime: created
-              category {
-                id
-                title
-                path
-              }
-              author {
-                id
-                name
-                image(width: 64, height: 64, fit: inside)
-                path
-              }
-            }
-          }
-        }
-      }
-    }  
-  }
-</page-query>
-
 <script>
-import CardItem from "~/components/Content/CardItem.vue";
-import Pagination from "~/components/Content/Pagination.vue";
-import ContentHeader from "~/components/Partials/ContentHeader.vue";
+import CardItem from '@/components/Content/CardItem'
+import Pagination from '@/components/Content/Pagination'
+import ContentHeader from '@/components/Partials/ContentHeader'
+
+let metaInfo = function() {
+  return {
+    title: this.$page.category.title
+  }
+}
+
+let postLabel = function() {
+  let pluralize = require("pluralize")
+  return pluralize("post", this.$page.category.belongsTo.totalCount)
+}
+
+let subTitle = function() {
+  return `A collection of ${this.$page.category.belongsTo.totalCount} ${this.postLabel}`
+}
 
 export default {
   components: {
@@ -76,19 +40,52 @@ export default {
     CardItem,
     ContentHeader
   },
+  metaInfo,
   computed: {
-    postLabel: function() {
-      var pluralize = require("pluralize");
-      return pluralize("post", this.$page.category.belongsTo.totalCount);
-    },
-    subTitle: function() {
-      return `A collection of ${this.$page.category.belongsTo.totalCount} ${this.postLabel}`;
-    }
-  },
-  metaInfo() {
-    return {
-      title: this.$page.category.title
-    };
+    postLabel,
+    subTitle
   }
-};
+}
 </script>
+
+<page-query>
+query($id: ID!, $page:Int) {
+  category(id: $id) {
+    title
+    path
+    belongsTo(perPage: 6, page: $page) @paginate {
+      totalCount
+      pageInfo {
+        totalPages
+        currentPage
+      }
+      edges {
+        node {
+          ... on Blog {
+            id
+            title
+            image(width: 800)
+            path
+            timeToRead
+            featured
+            humanTime: created(format: "DD MMM YYYY")
+            datetime: created
+            category {
+              id
+              title
+              path
+            }
+            author {
+              id
+              name
+              image(width: 64, height: 64, fit: inside)
+              path
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</page-query>
+
