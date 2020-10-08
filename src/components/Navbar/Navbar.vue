307132ff4bdfaf925a3810b5b1eb8603a80c720f
oomusou xiao <oomusou@gmail.com>
2020-10-08 23:28:19 +0800

    Refactor Navbar

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="h-16 dark:bg-black bg-white">
-    <headroom :classes="{'initial' : 'headroom bg-white dark:bg-black border-b dark:border-gray-900'}" @unpin="navbarUnpinned=true" @pin="navbarUnpinned=false">
-      <navbar-desktop v-on="$listeners" :theme="theme" :isShowSubnav="this.isNavbarPinned" @openSearchModal="onOpenSearchModal"/>
-      <navbar-mobile v-on="$listeners" :theme="theme" @openSearchModal="onOpenSearchModal" @openNavbarModal="onOpenNavbarModal"/>
+  <div class="h-16">
+    <headroom :classes="{'initial': 'headroom bg-white dark:bg-black border-b dark:border-gray-900'}" @pin="onPin" @unpin="onUnPin">
+      <navbar-desktop :theme="theme" :isShowSubNav="isNavbarPinned" @openSearchModal="onOpenSearchModal" v-on="$listeners"/>
+      <navbar-mobile :theme="theme" @openSearchModal="onOpenSearchModal" @openNavbarModal="onOpenNavbarModal" v-on="$listeners"/>
     </headroom>
-    <modal :showModal="this.showSearchModal" @close="onCloseSearchModal">
+    <modal :isShowModal="isShowSearchModal" @close="onCloseSearchModal">
       <search-modal></search-modal>
     </modal>
-    <modal :showModal="this.showNavbarModal" @close="onCloseNavbarModal">
+    <modal :isShowModal="isShowNavbarModal" @close="onCloseNavbarModal">
       <navbar-modal></navbar-modal>
     </modal>
   </div>
@@ -18,28 +18,36 @@ import { headroom } from 'vue-headroom'
 import NavbarDesktop from '@/components/Navbar/NavbarDesktop'
 import NavbarMobile from '@/components/Navbar/NavbarMobile'
 import Modal from '@/components/Modal/Modal'
-import SearchModal from '@/components/Modal/SearchModal'
 import NavbarModal from '@/components/Modal/NavbarMobileModal'
+import SearchModal from '@/components/Modal/SearchModal'
+
+let onUnPin = function() {
+  this.isNavbarPinned = false
+}
+
+let onPin = function() {
+  this.isNavbarPinned = true
+}
 
 let onOpenSearchModal = function() {
-  this.showSearchModal = true
+  this.isShowSearchModal = true
 }
 
 let onCloseSearchModal = function() {
-  this.showSearchModal = false
+  this.isShowSearchModal = false
 }
 
 let onOpenNavbarModal = function() {
-  this.showNavbarModal = true
+  this.isShowNavbarModal = true
 }
 
 let onCloseNavbarModal = function() {
-  this.showNavbarModal = false
+  this.isShowNavbarModal = false
 }
 
 let route_ = function() {
-  this.showNavbarModal = false
-  this.showSearchModal = false
+  this.isShowNavbarModal = false
+  this.isShowSearchModal = false
 }
 
 export default {
@@ -51,16 +59,18 @@ export default {
     NavbarModal,
     headroom
   },
-  props: [
-    'theme'
-  ],
+  props: {
+    theme: { default: '' }
+  },
   data: () => ({
-    showSearchModal: false,
-    showNavbarModal: false,
     headerHeight: 100,
+    isShowSearchModal: false,
+    isShowNavbarModal: false,
     isNavbarPinned: true
   }),
   methods: {
+    onUnPin,
+    onPin,
     onOpenSearchModal,
     onCloseSearchModal,
     onOpenNavbarModal,
