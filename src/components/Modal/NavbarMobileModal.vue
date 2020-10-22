2dbe64576acd334127e9efda064d2b316633f5ea
oomusou xiao <oomusou@gmail.com>
2020-10-22 08:05:42 +0800

    NavbarMobile may show menu

--- src/components/Modal/NavbarMobileModal.vue
+++ src/components/Modal/NavbarMobileModal.vue
@@ -5,45 +5,27 @@
       <div class="menu-links">
         <ul>
           <li v-for="navItem in $static.metadata.headerNavigation" :key="navItem.name" class="py-1">
-            <g-link class="block py-1" :to="navItem.link" :title="navItem.name" v-if="navItem.external!=true && navItem.children.length <=0">{{ navItem.name}}</g-link>
-            <a class="block" :href="navItem.link" target="_blank" :title="navItem.name" v-if="navItem.external==true && navItem.children.length <=0">{{ navItem.name}}</a>
-            <ClientOnly>
-              <v-popover placement="right" popoverClass="mobile-navbar-popover" offset="16" v-if="navItem.children.length > 0">
-                <a class="block py-1" style="cursor:pointer;">
-                  {{ navItem.name }}
-                  <font-awesome :icon="['fas', 'angle-right']"></font-awesome>
-                </a>
-
-                <template slot="popover">
-                  <ul>
-                    <li v-for="subItem in navItem.children" :key="subItem.name" class="px-4 py-2 submenu-item hover:text-white">
-                      <g-link class="block" :to="subItem.link" :title="subItem.name" v-if="subItem.external!==true">{{ subItem.name}}</g-link>
-                      <a class="block" :href="subItem.link" target="_blank" :title="subItem.name" v-if="subItem.external===true">{{ subItem.name}}</a>
-                    </li>
-                  </ul>
-                </template>
-              </v-popover>
-            </ClientOnly>
+            <g-link class="block py-1" :to="navItem.link" :title="navItem.name">{{ navItem.name }}</g-link>
           </li>
         </ul>
       </div>
     </div>
     <div class="mobileSubnav pl-2 dark:text-gray-400">
-      <subnavigation />
+      <subnavigation/>
     </div>
   </div>
 </template>
 
 <static-query>
 query {
-   metadata {
-    siteName
-    headerNavigation {
-      name
-      link
-      external
-    }
-  }
+metadata {
+siteName
+headerNavigation {
+name
+link
+external
+}
+}
 }
 </static-query>
 
